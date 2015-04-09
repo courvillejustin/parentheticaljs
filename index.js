4a0aa720f85c7a5e1257935041cd4095a54a0db5
unknown <CB0009@DEQMET00610D.state.mt.ads>
2015-04-09 13:29:04 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Given a string, find the start and end position of enclosing characters along with the inner content
+ * @return {Object} - contains the start, end, and inner content of enclosing characters
+ */
+var pMap = (function(){
+  return function(inputStr,openChar,closeChar){
+    openChar = openChar || '(';
+    closeChar = closeChar || ')';
+    var pDict = [];
+    var pStack = [];
+    inputStr.split('').forEach(function(c,pos){
+      if(c === openChar){
+        pStack.unshift(pos);
+      }
+      else if(c === closeChar){
+        var start = pStack.shift();
+        var end = pos;
+        pDict.push({
+          'start': start.toString(),
+          'end': end.toString(),
+          'content': inputStr.split('').slice(start + 1, end -1).join('')
+        });
+      }
+    });
+    return pDict;
+  };
+})();
+
+
+  var testString = "((Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.))";
+  console.log(pMap(testString));
